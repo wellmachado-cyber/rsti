d7de50d80ef45db94ffb9c39c41783deffeef87e
Isaac-Rangel <taliaegarota@gmail.com>
2024-08-15 16:48:08 -0300

    Arquivo novo feito

---
+++ well.js
@@ -0,0 +1,11 @@
+const amorzinhos = ["Renatinha Rodrigues", "Leonardo", "Arthur", "Ana", "Fernando", "Erick", "Guilherme", "Wellington", "Erick", "João Pedro", "Andrio"];
+
+class Amor {
+    constructor(array){
+        this.array = array;
+    }
+
+    amores(array){
+        return array;
+    }
+}
